cd9c735e3c282ba6858033d132d44623f75934f7
Scott Walsh <scott@invisiblethreat.ca>
2022-01-13 18:10:00 -0400

    Update for 2022-01-13

--- update.sh
+++ update.sh
@@ -4,10 +4,10 @@ today=$(date +%F)
 python3 openphish-pihole.py
 
 git add .
-git commit -m "Update for $today"
+commit=$(git commit -m "Update for $today" 2>&1)
 output=$(git push 2>&1)
 
 # let cron generate a message if things go poorly
 if [ "$?" != "0" ]; then
-  echo $output
+  echo "Commit message:\n$commit\n\nPush response:\n$output"
 fi
