b5c1605e123963cc335b4e182a515b8f521a446c
Scott Walsh <scott@invisiblethreat.ca>
2022-04-05 20:37:03 -0300

    Update for 2022-04-05

--- update.sh
+++ update.sh
@@ -10,4 +10,6 @@ output=$(git push 2>&1)
 # let cron generate a message if things go poorly
 if [ "$?" != "0" ]; then
   echo "Commit message:\n$commit\n\nPush response:\n$output"
+else
+  curl -s 'http://172.18.0.35:3001/api/push/8aQF38JkuW?msg=OK&ping=' > /dev/null
 fi
